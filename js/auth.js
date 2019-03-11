4faa04903965dfe37737b456a4256adf4c9777b5
pranavtharoor <pranav.tharoor@gmail.com>
2019-03-12 02:04:00 +0530

    Added main website

---
+++ js/auth.js
@@ -0,0 +1,7 @@
+function changeAuth() {
+  document.querySelector("#auth").classList.toggle("login-active");
+  document.querySelector("#auth").classList.toggle("register-active");
+  document.querySelector("#auth-details").outerHTML = document.querySelector(
+    "#auth-details"
+  ).outerHTML;
+}
