5f12b087499b4c5e83e7c75bd133090b3a7017c8
pranavtharoor <pranav.tharoor@gmail.com>
2019-03-17 15:43:10 +0530

    Integrated auth page

--- public/js/auth.js
+++ public/js/auth.js
@@ -5,3 +5,59 @@ function changeAuth() {
     '#auth-details'
   ).outerHTML;
 }
+
+onload = () => {
+  document.querySelector('#reg-form').addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.querySelector('#reg-form input[name="name"]').value;
+    const email = document.querySelector('#reg-form input[name="email"]').value;
+    const mobile = document.querySelector('#reg-form input[name="mobile"]')
+      .value;
+    const password = document.querySelector('#reg-form input[name="password"]')
+      .value;
+    fetch('/api/auth/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, email, mobile, password })
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        snackbar(data.msg, data.success);
+        if (data.success) changeAuth();
+        document.querySelector('#login-form input[name="email"]').value = email;
+        document.querySelector('#login-form input[name="password"]').focus();
+      });
+  });
+  document.querySelector('#login-form').addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.querySelector('#login-form input[name="email"]')
+      .value;
+    const password = document.querySelector(
+      '#login-form input[name="password"]'
+    ).value;
+    fetch('/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        snackbar(data.msg, data.success);
+        if (data.success) window.location.href = '/events.html';
+      });
+  });
+};
+
+function snackbar(content, success = true) {
+  if (!content) return;
+  document.querySelector('#snackbar').classList.remove('success');
+  document.querySelector('#snackbar').classList.remove('danger');
+  document.querySelector('#snackbar').innerHTML = content;
+  document.querySelector('#snackbar').classList.add('open');
+  document
+    .querySelector('#snackbar')
+    .classList.add(success ? 'success' : 'danger');
+  setTimeout(() => {
+    document.querySelector('#snackbar').classList.remove('open');
+  }, 4000);
+}
