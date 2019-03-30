45e0db07d75fe70e75ec11e8e3dc88d9be77ad41
ekam walia <ekamwalia@gmail.com>
2019-03-30 16:58:10 +0530

    Fixed bugs

--- public/js/results.js
+++ public/js/results.js
@@ -16,10 +16,7 @@ window.onload = function () {
     recordScore.addEventListener('click', function (e) {
         e.preventDefault();
 
-        const eventName = document.getElementById('recordScoreEventNum').value;
-        const score = document.getElementById('recordScoreScore').value;
-        const round = document.getElementById('recordScoreRound').value;
-        const event = events[eventName];
+
 
         let scanner = new Instascan.Scanner({
             continuous: true,
@@ -32,6 +29,11 @@ window.onload = function () {
         });
 
         scanner.addListener('scan', function (qr) {
+            const eventName = document.getElementById('recordScoreEventNum').value;
+            const score = document.getElementById('recordScoreScore').value;
+            const round = document.getElementById('recordScoreRound').value;
+            const event = events[eventName];
+
             fetch('/api/results/record', {
                 credentials: 'include',
                 method: 'POST',
@@ -54,45 +56,45 @@ window.onload = function () {
             });
     })
 
-    markAttendance.addEventListener('click', function (e) {
-        e.preventDefault();
-        const eventName = document.getElementById('markAttendanceEventNum').value;
-        const round = document.getElementById('markAttendanceRound').value;
-        const score = 0;
-        const event = events[eventName];
+    // markAttendance.addEventListener('click', function (e) {
+    //     e.preventDefault();
 
-        let scanner = new Instascan.Scanner({
-            continuous: true,
-            video: document.getElementById('preview'),
-            mirror: false,
-            backgroundScan: false,
-            captureImage: false,
-            refactoryPeriod: 5000,
-            scanPeriod: 1
-        });
+    //     let scanner = new Instascan.Scanner({
+    //         continuous: true,
+    //         video: document.getElementById('preview'),
+    //         mirror: false,
+    //         backgroundScan: false,
+    //         captureImage: false,
+    //         refactoryPeriod: 5000,
+    //         scanPeriod: 1
+    //     });
 
-        scanner.addListener('scan', function (qr) {
-            fetch('/api/results/record', {
-                credentials: 'include',
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ qr, event, score, round })
-            })
-                .then(resp => resp.json())
-                .then(data => {
-                    alert(data.msg)
-                });
-        });
+    //     scanner.addListener('scan', function (qr) {
+    //         const eventName = document.getElementById('markAttendanceEventNum').value;
+    //         const round = document.getElementById('markAttendanceRound').value;
+    //         const score = 0;
+    //         const event = events[eventName];
+    //         fetch('/api/results/record', {
+    //             credentials: 'include',
+    //             method: 'POST',
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify({ qr, event, score, round })
+    //         })
+    //             .then(resp => resp.json())
+    //             .then(data => {
+    //                 alert(data.msg)
+    //             });
+    //     });
 
-        Instascan.Camera.getCameras()
-            .then(function (cameras) {
-                if (cameras.length > 0) scanner.start(cameras[cameras.length - 1]);
-                else console.error('No cameras found');
-            })
-            .catch(function (e) {
-                console.error(e);
-            });
-    })
+    //     Instascan.Camera.getCameras()
+    //         .then(function (cameras) {
+    //             if (cameras.length > 0) scanner.start(cameras[cameras.length - 1]);
+    //             else console.error('No cameras found');
+    //         })
+    //         .catch(function (e) {
+    //             console.error(e);
+    //         });
+    // })
 
     updateScore.addEventListener('click', function (e) {
         e.preventDefault();
